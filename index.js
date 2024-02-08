98fe3b84ec149411964c457407eb7b2d2611dd35
akukamil <chamilioner_kid@mail.ru>
2024-02-08 17:18:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2074,13 +2074,7 @@ auth2 = {
 			
 			if (my_data.name === '')
 				my_data.name = this.get_random_name(my_data.uid);
-			
-			//если английский яндекс до добавляем к имени страну
-			let country_code = await this.get_country_code();
-			my_data.name = my_data.name + ' (' + country_code + ')';			
-
-
-			
+						
 			return;
 		}
 		
@@ -2106,9 +2100,8 @@ auth2 = {
 		
 		if (game_platform === 'GOOGLE_PLAY') {	
 
-			let country_code = await this.get_country_code();
 			my_data.uid = this.search_in_local_storage() || this.get_random_uid_for_local('GP_');
-			my_data.name = this.get_random_name(my_data.uid) + ' (' + country_code + ')';
+			my_data.name = this.get_random_name(my_data.uid) ;
 			my_data.pic_url = 'https://avatars.dicebear.com/api/adventurer/' + my_data.uid + '.svg';	
 			return;
 		}
